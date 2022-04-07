7e48a38fb6110e07a76bc017bff1e90ea0dadbe7
Joris Kamminga <joris.kamminga@gmail.com>
2022-04-07 20:54:58 +0200

    Finished add board modal design

--- src/components/helpers/Modal.js
+++ src/components/helpers/Modal.js
@@ -1,8 +1,44 @@
-const Modal = ({ open = true }) => {
-  return (
-    <div className={`modal_overlay animate__animated ${open ? "animate__fadeIn" : "animate__fadeOut"}`}>
-      <div className={`modal ${open ? "animate__bounceIn" : "animate__bounceOut"}`}>
+import { useEffect, useRef } from "react";
 
+let justOpened = false;
+
+const Modal = ({ open = true, handleClose, children }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (
+        e.target !== modalRef?.current &&
+        !modalRef?.current?.contains(e.target) &&
+        !justOpened
+      ) {
+        handleClose();
+      }
+    }
+
+    if (open) {
+      justOpened = true;
+      setTimeout(() => (justOpened = false), 200);
+
+      document.addEventListener("click", handleClick);
+    } else {
+      document.removeEventListener("click", handleClick);
+    }
+
+    return () => document.removeEventListener("click", handleClick);
+  }, [open]);
+
+  return (
+    <div
+      className={`modal_overlay animate__animated ${
+        open ? "animate__fadeIn" : "hidden animate__fadeOut"
+      }`}
+    >
+      <div
+        ref={modalRef}
+        className={`modal ${open ? "animate__zoomIn" : "animate__zoomOut"}`}
+      >
+        {children}
       </div>
     </div>
   );
--- src/pages/BoardListPage.js
+++ src/pages/BoardListPage.js
@@ -1,14 +1,39 @@
 import _ from "lodash";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CustomSelect from "../components/helpers/CustomSelect";
 import Modal from "../components/helpers/Modal";
 import { FormatTime } from "../utils/helpers/common.helpers";
+import { ReactComponent as BoardPreview } from "../assets/board-preview.svg";
+import FormField from "../components/form/FormField";
+import { CheckField } from "../utils/helpers/validation.helpers";
 
 const filterOptions = [
   { name: "Most recently active", abbr: "activity-desc", active: true },
   { name: "Last recently active", abbr: "activity-asc" },
   { name: "Alphabetically A-Z", abbr: "alpha-asc" },
   { name: "Alphabetically Z-A", abbr: "alpha-desc" },
+];
+
+const boardsList = [
+  {
+    id: "7a98a16a-b665-43ee-929e-e59d7060f3c1",
+    title: "Board",
+    color: "#a55eea",
+    members: [],
+    lastUpdated: 1649098893480,
+    favorited: true,
+  },
+];
+
+const backgroundColors = [
+  "#eb3b5a",
+  "#fa8231",
+  "#f7b731",
+  "#20bf6b",
+  "#0fb9b1",
+  "#45aaf2",
+  "#4b7bec",
+  "#a55eea",
 ];
 
 const BoardsFilter = () => {
@@ -44,19 +69,87 @@ const BoardsSearch = ({ search, setSearch }) => {
   );
 };
 
-const boardsList = [
-  {
-    id: "7a98a16a-b665-43ee-929e-e59d7060f3c1",
-    title: "Board",
-    color: "#a55eea",
-    members: [],
-    lastUpdated: 1649098893480,
-    favorited: true,
-  },
-];
+const AddBoardModal = ({ open, handleClose }) => {
+  const [state, setState] = useState(setInitialState());
+  const [touched, setTouched] = useState(false);
+
+  const titleField = {
+    label: "Title",
+    value: state.title,
+    touched,
+    optional: false,
+    fullWidth: true,
+  };
+
+  useEffect(() => {
+    if (!open) {
+      setState(setInitialState());
+      setTouched(false);
+    }
+  }, [open]);
+
+  function setInitialState() {
+    return { title: "", color: backgroundColors[0] };
+  }
+
+  return (
+    <Modal open={open} handleClose={handleClose}>
+      <div className="modal_header">
+        <div className="modal_header_title">Create board</div>
+        <button className="modal_close_btn">
+          <i className="fa-regular fa-xmark"></i>
+        </button>
+      </div>
+      <div className="modal_content">
+        <div className="add_board_preview_wrapper">
+          <div
+            style={{ backgroundColor: state.color }}
+            className="add_board_preview"
+          >
+            <BoardPreview />
+          </div>
+        </div>
+        <div className="add_board_fields">
+          <div className="add_board_background_field">
+            <label className="form_field_label">Background</label>
+            <ul className="board_background_option_list">
+              {backgroundColors.map((color, index) => (
+                <li
+                  key={`board-background-${index}`}
+                  className="board_background_option_wrapper"
+                >
+                  <button
+                    style={{ backgroundColor: color }}
+                    className={`board_background_option ${
+                      color === state.color ? "active" : ""
+                    }`}
+                    onClick={() => setState({ ...state, color })}
+                  ></button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <FormField
+            formName="add-board"
+            field={titleField}
+            handleChange={(e) => setState({ ...state, title: e.target.value })}
+            handleTouch={(name, touched) => setTouched(touched)}
+          />
+          <button
+            disabled={state.title === "" || CheckField()}
+            className="add_board_submit_btn"
+          >
+            Create board
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
 const BoardListPage = () => {
   const [boards, setBoards] = useState(boardsList);
+  const [open, setOpen] = useState(false);
 
   function handleFavorite(boardId) {
     const updatedBoards = _.cloneDeep(boards);
@@ -70,6 +163,14 @@ const BoardListPage = () => {
     setBoards(updatedBoards);
   }
 
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
     <div className="page_content">
       <div className="page_title">Your boards</div>
@@ -79,14 +180,16 @@ const BoardListPage = () => {
       </div>
       <div className="boards_list_count">Showing 1 of 1 boards</div>
       <div className="boards_list">
-        <div className="board_btn_wrapper add_board">
+        <div className="board_btn_wrapper add_board" onClick={handleOpen}>
           <div className="add_board_btn_text">Create new board</div>
         </div>
         {boards.map((board) => (
           <div
             key={`board-${board?.id}`}
             style={{ backgroundColor: board?.color }}
-            className={`board_btn_wrapper ${board?.favorited ? "favorite" : ""}`}
+            className={`board_btn_wrapper ${
+              board?.favorited ? "favorite" : ""
+            }`}
           >
             <div>
               <div className="board_btn_title">{board?.title}</div>
@@ -134,6 +237,7 @@ const BoardListPage = () => {
           </div>
         ))}
       </div>
+      <AddBoardModal open={open} handleClose={handleClose} />
     </div>
   );
 };
