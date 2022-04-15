007851c1df72aa7a27d2a2dd9c8cc29f742727d0
Joris Kamminga <joris.kamminga@gmail.com>
2022-04-15 15:51:51 +0200

    removed unused import

--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const navigation = [
